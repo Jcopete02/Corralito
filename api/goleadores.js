273d3340fb7ec28db5ce10602d1d5ff83f7fd0e7
jhoimarcopete <copetejhoimar@gmail.com>
2025-09-08 00:17:32 -0500

    Corregir sintaxis de API

--- api/goleadores.js
+++ api/goleadores.js
@@ -11,7 +11,7 @@ const dbConfig = {
     }
 };
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
